f3b586cc4d677308187f53d3d6895f51d710b0b2
jeyog <choijy8816@gmail.com>
2023-06-14 13:42:54 +0900

    change indentation

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,14 @@
 import { renderStatic } from '$lib/server/renderer'
 
 export const handle = (async ({ event, resolve }) => {
-	const response = await resolve(event, {
-		transformPageChunk: async ({ html }) => {
-			const { css, ids } = await renderStatic(html)
-			const style = `<style data-emotion-css="${ids.join(' ')}">${css}</style>`
-			return html.replace('%sveltekit.style%', style)
-		}
-	})
+  const response = await resolve(event, {
+    transformPageChunk: async ({ html }) => {
+      const { css, ids } = await renderStatic(html)
+      const style = `<style data-emotion-css="${ids.join(' ')}">${css}</style>`
+      
+      return html.replace('%sveltekit.style%', style)
+    }
+  })
 
-	return response
+  return response
 })
